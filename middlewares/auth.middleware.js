e1aac406ab3c9553a5f6280fe897aff4750c9a69
LisaAlisonDev <alisonbarretpro@gmail.com>
2024-01-03 09:52:06 +0400

    CS

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-function authenticateToken(req, res, next) {
+exports.authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
   
